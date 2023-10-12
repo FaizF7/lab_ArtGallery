885d9efe119801ee3a6e605ef25b65d03c610f2d
Thomas McLaughlin <tmclaughlin5.1111@gmail.com>
2023-10-12 16:35:03 +0100

    getters/setters

--- src/main/java/ArtGallery.java
+++ src/main/java/ArtGallery.java
@@ -6,7 +6,33 @@ public class ArtGallery {
     private ArrayList<Artwork> stock; // stock
 
     public ArtGallery(String name, double till){
+        this.name = name;
+        this.till = till;
 
+        this.stock = new ArrayList<>(); // empty arraylist
     }
 
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getTill() {
+        return this.till;
+    }
+
+    public void setTill(double till) {
+        this.till = till;
+    }
+
+    public ArrayList<Artwork> getStock() {
+        return this.stock;
+    }
+
+    public void setStock(ArrayList<Artwork> stock) {
+        this.stock = stock;
+    }
 }
